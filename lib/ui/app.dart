566bd1cb9adbaeb608b8050712d48a9ab6a55ae2
Konrad <konradkrol110@gmail.com>
2024-06-27 13:54:30 +0200

    Setup l10n and GitHub Actions

---
+++ lib/ui/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: Scaffold(
+        body: Center(
+          child: Builder(builder: (context) {
+            return Text(AppLocalizations.of(context)!.helloWorld);
+          }),
+        ),
+      ),
+    );
+  }
+}
