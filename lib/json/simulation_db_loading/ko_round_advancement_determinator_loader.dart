aec681c5cafd10ba23f1b2d0dc93a51da1943eff
Konrad <konradkrol110@gmail.com>
2024-08-22 19:40:46 +0200

    Add a serializer and loader for SomethingCreator classes from competition rules (without changing cometition and classification rules serializing yet)

---
+++ lib/json/simulation_db_loading/ko_round_advancement_determinator_loader.dart
@@ -0,0 +1,25 @@
+import 'package:sj_manager/json/simulation_db_loading/simulation_db_part_loader.dart';
+import 'package:sj_manager/json/json_types.dart';
+import 'package:sj_manager/models/simulation_db/competition/rules/utils/ko_round_advancement_determinator/concrete/n_best.dart';
+import 'package:sj_manager/models/simulation_db/competition/rules/utils/ko_round_advancement_determinator/ko_round_advancement_determinator.dart';
+import 'package:sj_manager/repositories/generic/items_ids_repo.dart';
+
+class KoRoundAdvancementDeterminatorLoader
+    implements SimulationDbPartParser<KoRoundAdvancementDeterminator> {
+  const KoRoundAdvancementDeterminatorLoader({
+    required this.idsRepo,
+  });
+
+  final ItemsIdsRepo idsRepo;
+
+  @override
+  KoRoundAdvancementDeterminator load(Json json) {
+    final type = json['type'] as String;
+    return switch (type) {
+      'n_best' => const NBestKoRoundAdvancementDeterminator(),
+      _ => throw UnsupportedError(
+          '(Loading) An unsupported KoRoundAdvancementDeterminator type ($type)',
+        ),
+    };
+  }
+}
---
+++ lib/json/simulation_db_saving/ko_groups_creator_serializer.dart
@@ -0,0 +1,48 @@
+import 'package:sj_manager/json/simulation_db_saving/simulation_db_part_serializer.dart';
+import 'package:sj_manager/json/json_types.dart';
+import 'package:sj_manager/models/simulation_db/competition/rules/utils/ko_group_creator.dart/concrete/default_classic.dart';
+import 'package:sj_manager/models/simulation_db/competition/rules/utils/ko_group_creator.dart/concrete/default_random.dart';
+import 'package:sj_manager/models/simulation_db/competition/rules/utils/ko_group_creator.dart/concrete/default_with_pots.dart';
+import 'package:sj_manager/models/simulation_db/competition/rules/utils/ko_group_creator.dart/ko_groups_creator.dart';
+import 'package:sj_manager/repositories/generic/items_ids_repo.dart';
+
+class KoGroupsCreatorSerializer implements SimulationDbPartSerializer<KoGroupsCreator> {
+  const KoGroupsCreatorSerializer({
+    required this.idsRepo,
+  });
+
+  final ItemsIdsRepo idsRepo;
+
+  @override
+  Json serialize(KoGroupsCreator creator) {
+    if (creator is DefaultClassicKoGroupsCreator) {
+      return _parseDefaultClassic(creator);
+    } else if (creator is DefaultRandomKoGroupsCreator) {
+      return _parseDefaultRandom(creator);
+    } else if (creator is DefaultPotsKoGroupsCreator) {
+      return _parseDefaultPots(creator);
+    } else {
+      throw UnsupportedError(
+        '(Parsing) An unsupported KoGroupsCreator type (${creator.runtimeType})',
+      );
+    }
+  }
+
+  Json _parseDefaultClassic(DefaultClassicKoGroupsCreator averager) {
+    return {
+      'type': 'default_classic',
+    };
+  }
+
+  Json _parseDefaultRandom(DefaultRandomKoGroupsCreator averager) {
+    return {
+      'type': 'default_random',
+    };
+  }
+
+  Json _parseDefaultPots(DefaultPotsKoGroupsCreator averager) {
+    return {
+      'type': 'default_pots',
+    };
+  }
+}
