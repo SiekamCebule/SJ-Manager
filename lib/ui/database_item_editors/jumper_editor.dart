76f993bfcfb82b872c95e1c5edf22e8f7c3fda0a
Konrad Król <konradkrol110@gmail.com>
2024-08-27 08:01:55 +0200

    Make it works on Windows

--- lib/ui/database_item_editors/jumper_editor.dart
+++ lib/ui/database_item_editors/jumper_editor.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 import 'package:sj_manager/l10n/helpers.dart';
